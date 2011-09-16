9416fd410285f01f2daba95bf74f73133fca168c
Daniel MacDougall <dmacdougall@gmail.com>
2011-09-16 16:18:13 -0700

    clean up example app

--- app.rkt
+++ app.rkt
@@ -3,18 +3,27 @@
 (require "spin.rkt"
          web-server/templates)
 
-(get "/" (lambda () "Index"))
+(get "/" (lambda () "GET request"))
+
+(post "/" (lambda () "POST request"))
+
+(put "/" (lambda () "PUT request"))
+
+(delete "/" (lambda () "DELETE request"))
 
 (get "/hi/:name" (lambda (req)
   (string-append "Hi, " (params req 'name))))
 
-(get "/headers" (lambda (req)
-  (define h (header #"X_HTTP_CUSTOM" #"Custom header!"))
-  (list 200 (list h) "Check out the custom header")))
-
-(get "/hi/:first_name/and/:second_name" (lambda (req)
+(get "/hi/:first_name/:last_name" (lambda (req)
   (define first_name (params req 'first_name))
-  (define second_name (params req 'second_name))
+  (define last_name (params req 'last_name))
   (include-template "index.html")))
+
+(get "/headers" (lambda (req)
+  (define h (header #"Custom-Header" #"This is a custom header"))
+  `(200 (,h) "Check out the custom header")))
+
+(post "/payload" (lambda (req)
+  (string-append "POSTed payload: " (params req 'payload))))
 
 (run)
