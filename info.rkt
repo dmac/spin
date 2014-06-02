1c7f7887bfa1cb976bed24ceb935d986ac2b107f
Daniel MacDougall <dmacdougall@gmail.com>
2014-06-02 15:02:45 -0700

    Bump version and add release notes

--- info.rkt
+++ info.rkt
@@ -3,11 +3,12 @@
 (define blurb
   (list '(p "Write RESTful web apps in Racket.")))
 (define release-notes
-  (list '(ul (li "Update package with minimum required Racket version (5.2)"))))
+  (list '(ul (li "Support for serve/servlet args")
+             (li "Customizable response handlers"))))
 (define primary-file "main.rkt")
 (define categories '(net))
 (define homepage "https://github.com/dmacdougall/spin")
-(define version "1.2")
+(define version "1.3")
 (define required-core-version "5.2")
 (define repositories
   (list "4.x"))
